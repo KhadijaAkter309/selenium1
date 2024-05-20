e96accd1901060524e03c519a02db4cc821fe630
Administrator <akterkhadija309@gmail.com>
2024-05-20 12:22:30 +0600

    first commit

---
+++ src/test/java/seleniumProject/BrowserLaunch.java
@@ -0,0 +1,19 @@
+package seleniumProject;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserLaunch {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://mvnrepository.com/");
+        driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
+
+    }
+}
